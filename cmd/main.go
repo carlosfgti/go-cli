0208f16fa2acc188544b5a0f7c9472e5699b4b00
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-09-04 08:56:46 -0300

    feat: cli interactive with currency money

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	const apiURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+	const apiBaseURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies="
+
+	fmt.Print("Enter currency (ex: usd, eur, brl): ")
+	var currency string
+	fmt.Scanln(&currency)
+	apiURL := fmt.Sprintf("%s%s", apiBaseURL, strings.ToLower(currency))
 
 	type PriceResponse struct {
-		Bitcoin struct {
-			USD float64 `json:"usd"`
-		} `json:"bitcoin"`
+		Bitcoin map[string]float64 `json:"bitcoin"`
 	}
 
 	resp, err := http.Get(apiURL)
@@ -32,6 +36,11 @@ func main() {
 		fmt.Println("Fail decode response:", err)
 		panic(err)
 	}
+	price, ok := processPrice.Bitcoin[strings.ToLower(currency)]
+	if !ok {
+		fmt.Println("Fail get price")
+		panic("Fail get price")
+	}
 
-	fmt.Printf("Bitcoin price: $%.2f\n", processPrice.Bitcoin.USD)
+	fmt.Printf("Bitcoin price in %s is: %.2f\n", strings.ToUpper(currency), price)
 }
